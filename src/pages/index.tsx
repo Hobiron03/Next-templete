aa9c881d68e59946e17e9d9cf29c3104ea18ad11
Yudai Kawakami <yudainepnep@icloud.com>
2022-08-07 23:11:21 +0900

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,37 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import { useMediaQuery, useViewportSize } from "src/lib/mantine";
+
+import { Button } from "@mantine/core";
+
+const Home: NextPage = () => {
+  const { width } = useViewportSize();
+  const largerThanXs = useMediaQuery("xs");
+  const largerThanSm = useMediaQuery("sm");
+  const largerThanMd = useMediaQuery("md");
+  const largerThanLg = useMediaQuery("lg");
+  const largerThanXl = useMediaQuery("xl");
+
+  const handleClick = () => {
+    console.log("Hello!");
+  };
+
+  return (
+    <div className="p-20">
+      <div className="bg-fuchsia-200 xs:bg-red-200 sm:bg-amber-200 md:bg-lime-200 lg:bg-emerald-200 xl:bg-cyan-200">
+        <div>{`width: ${width}`}</div>
+        <div>{`largerThanXs: ${largerThanXs}`}</div>
+        <div>{`largerThanSm: ${largerThanSm}`}</div>
+        <div>{`largerThanMd: ${largerThanMd}`}</div>
+        <div>{`largerThanLg: ${largerThanLg}`}</div>
+        <div>{`largerThanXl: ${largerThanXl}`}</div>
+      </div>
+      <Button onClick={handleClick} className="mt-4 block">
+        Click me!
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
